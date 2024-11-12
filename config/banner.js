8439ef065c4bdd5199ad4508c740f4b67cb92115
Byte Beggar <rahmacutee07@gmail.com>
2024-11-12 20:29:06 +0800

    Create banner.js

---
+++ config/banner.js
@@ -0,0 +1,24 @@
+const figlet = require("figlet");
+const { ColorTheme } = require("./colors");
+
+const colors = new ColorTheme();
+
+function displayBanner() {
+  const banner = figlet.textSync("Humanity BOT", {
+    font: "ANSI Shadow",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 100,
+  });
+
+  console.log(colors.style(banner, "header"));
+  console.log(
+    colors.style("===============================================", "border")
+  );
+  console.log(colors.style("GitHub  : https://github.com/ByteBeggar", "link"));
+  console.log(
+    colors.style("===============================================\n", "border")
+  );
+}
+
+module.exports = displayBanner;
